6bcb633614e28143c1d2f16e9bc5fe2f0798ff25
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-07 08:34:55 -0500

    Agregue mensaje de error al HTML y le di funcionalidad para que no se repita

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 //Variables para campos
 const email = document.querySelector('#email');
@@ -31,5 +32,20 @@ function validarFormulario(e) {
         console.log('Si hay algo');
     } else {
         e.target.classList.add('border', 'border-red-500');
+
+        mostrarError();
     }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-color-red-100', 'text-red-500', 'p-3', 'text-center', 'mt-5', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if ( errores.length === 0 ) {
+        formulario.appendChild(mensajeError);
+    }
+
+
 }
