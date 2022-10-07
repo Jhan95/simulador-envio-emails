9b295f86749e072d56e847b0434b7e280af4d0d6
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-07 11:17:44 -0500

    Tratando de arreglar el problema de la validacion de error

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Variables
+//Varriables
 const btnEnviar = document.querySelector('#enviar');
 const formulario = document.querySelector('#enviar-mail');
 
@@ -13,10 +13,13 @@ eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', iniciarApp);
 
+    //Campos del formulario
     email.addEventListener('blur', validarFormulario);
     asunto.addEventListener('blur', validarFormulario);
     mensaje.addEventListener('blur', validarFormulario);
+
 }
+
 
 
 //Funciones
@@ -28,30 +31,22 @@ function iniciarApp() {
 //Valida el formulario
 function validarFormulario(e) {
 
-    if(e.target.value.length > 0) {
+    if(  e.target.value.length > 0 ) {
         console.log('Si hay algo');
     } else {
         e.target.classList.add('border', 'border-red-500');
 
-        mostrarError('Todos los campos son obligatorios');
-    }
-
-    if( e.target.type === 'email' ) {
-        const resultado = e.target.value.indexOf('@');
-
-        if( resultado < 0 ) {
-            mostrarError('El Email no es válido');
-        }
+        mostrarError();
     }
 }
 
-function mostrarError(mensaje) {
+function mostrarError() {
     const mensajeError = document.createElement('p');
-    mensajeError.textContent = mensaje;
-    mensajeError.classList.add('border', 'border-red-500', 'background-color-red-100', 'text-red-500', 'p-3', 'text-center', 'mt-5', 'error');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'text-center', 'mt-5', 'error');
 
     const errores = document.querySelectorAll('.error');
-    if ( errores.length === 0 ) {
+    if( errores.length === 0 ) {
         formulario.appendChild(mensajeError);
     }
 
