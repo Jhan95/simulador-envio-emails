ecf93584a25fecc08b1055e6aaf68e85af251c19
Jhan Vásquez <jhanvasquez95@gmail.com>
2022-10-07 08:23:07 -0500

    Validacion de los campos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 //Variables
 const btnEnviar = document.querySelector('#enviar');
 
+//Variables para campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 
 //Eventos
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -13,4 +22,14 @@ function eventListeners() {
 function iniciarApp() {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+//Valida el formulario
+function validarFormulario(e) {
+
+    if(e.target.value.length > 0) {
+        console.log('Si hay algo');
+    } else {
+        e.target.classList.add('border', 'border-red-500');
+    }
 }
